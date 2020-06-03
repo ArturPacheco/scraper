5e3d9441bf332284efd7699180a186feea87d38a
Artur Pacheco <arturpacheko@gmail.com>
2020-06-03 00:29:22 -0300

    Adding console logs of the HTML when "R$" is found

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-//DX RACER
+//DX RACER - JSON
 /*
 (async () => {
     let pageUrl = 'https://www.dxracer.com.br/cadeira-dxracer-racing-rw01n-8-p985975';
@@ -32,7 +32,7 @@ const fs = require('fs');
 })();
 
 
-//KABUM ALPHA GAMER
+//KABUM ALPHA GAMER - JSON
 (async () => {
     let pageUrl = 'https://www.kabum.com.br/produto/93907/cadeira-gamer-alpha-gamer-polaris-racing-black';
 
@@ -64,49 +64,49 @@ const fs = require('fs');
 })();
 */
 
-//EXTRACAO DE HTML - DXRACER
+//DXRACER - PUPPETEER HTML EXTRACT
 (async () => {
-    let pageUrl = 'https://www.dxracer.com.br/cadeira-dxracer-racing-rw01n-8-p985975';
+    let pageUrl = 'https://produto.mercadolivre.com.br/MLB-1337187769-banco-rustico-banquinho-madeira-rustica-banco-pequeno-_JM?quantity=1&variation=53220388316&onAttributesExp=true#position=21&type=item&tracking_id=ae62ce72-41bb-4403-9f2f-71a7178ad016';
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(pageUrl, { waitUntil: 'networkidle2' });
 
     const html = await page.content();
-    //console.log(html);
 
-    fs.writeFile("output.html", html, function(err) {
+    fs.writeFileSync("output.html", html, function(err) {
         if(err) {
             return console.log(err);
         }
         console.log("The file was saved!");
-    }); 
+    });
+
+    let htmlFile = fs.readFileSync("output.html", "utf8");
+    let arr = htmlFile.split(/\r?\n/);
+    arr.forEach((line, idx) => {
+        if(line.includes("R$")){
+            console.log((idx+1)+':'+ line);
+        }
+    });
 
     await browser.close();
 
 })();
 
 
-//EXTRACAO DE HTML - OUTRO
+//DX RACER - CHEERIO
 /*
-(async () => {
-    let pageUrl = 'https://www.magazineluiza.com.br/papel-higienico-folha-dupla-neve-toque-de-seda-24-rolos-30m/p/215282300/me/pahi/';
-
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(pageUrl, { waitUntil: 'networkidle2' });
-
-    const html = await page.content();
-    //console.log(html);
-
-    fs.writeFile("output.html", html, function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    }); 
-
-    await browser.close();
-
-})();
+let options = {
+    url: 'https://www.dxracer.com.br/cadeira-dxracer-racing-rw01n-8-p985975',
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+    }
+};
+request(options, (error, response, html) => {
+    if(!error && response.statusCode == 200){
+        console.log(html.length);
+    }else{
+        console.log(response.statusCode);
+    }
+});
 */
