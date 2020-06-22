40b4223641875f43fd7e97175915b5d0b5be95be
Artur Pacheco <arturpacheko@gmail.com>
2020-06-21 22:30:16 -0300

    Created the list as an objects array.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-    let pageUrl = 'https://www.ultrafarma.com.br/cimegripe-com-20-capsulas-validade-julho-2019';
+    let pageUrl = 'https://lojamelissa.com.br/mini-melissa-possession#options=13167,10335';
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -20,29 +20,26 @@ const fs = require('fs');
     let htmlFile = fs.readFileSync("output.html", "utf8");
     let arr = htmlFile.split(/\r?\n/);
     let reaisRe = new RegExp(/(?:[1-9]\d{0,2}(?:\.\d{3})*),\d{2}/g);
-    let matchList = {};
-    let matchListIdx = 0;
+    let matchList = [];
     arr.forEach((line, idx) => { //Percorre linha por linha do html
         if (line.match(reaisRe)){ //Se encontrar algo no formato de reais na linha em questão
             line.match(reaisRe).forEach((lineFoundValue)=>{ //Percorre os valores encontrados na linha
-                let data = { //Cria uma estrutura com as informações daquele valor
+                matchList.push({
                     foundValue: lineFoundValue,      //O próprio valor
                     htmlLine: (idx+1),               //A linha encontrada
                     htmlPrevLineContent: arr[idx-1], //O conteúdo da linha anterior
                     htmlLineContent: line,           //O conteúdo da linha
                     htmlNextLineContent: arr[idx+2]  //O conteúdo da linha seguinte
-                };
-                matchList[matchListIdx] = []; //Inicializa a posição do array de valores encontrados
-                matchList[matchListIdx].push(data); //Adiciona o novo registro junto aos outros valores encontrados
-                matchListIdx++; //+1 para o índice de valores encontrador
+                });
             });
         }
     });
 
-    //console.log(matchList[5]);
-    //console.log(matchList[6]);
-    //console.log(matchList[7]);
-    console.log("Foram encontrados " + (matchListIdx+1) +" valores em reais na página.");
+    
+    //matchList.forEach()
+
+    console.log(matchList);
+    console.log("Foram encontrados " + (matchList.length+1) +" valores em reais na página.");
 
     await browser.close();
 })();
