3bc6bc4dba3e5073b995429f4fdbce85ad921d9b
Artur Pacheco <arturpacheko@gmail.com>
2020-08-06 00:29:27 -0300

    New model using XPATH

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const { measureMemory } = require('vm');
+const { html } = require('cheerio');
 
 (async () => {
     //let pageUrl = 'https://www.mercadolivre.com.br/moto-g8-plus-dual-sim-64-gb-cosmic-blue-4-gb-ram/p/MLB15273216?source=search#searchVariation=MLB15273216&position=1&type=product&tracking_id=e65178bc-1e6e-49a1-9e24-8a9cd86698b0';
-    //let pageUrl = 'https://www.dxracer.com.br/cadeira-dxracer-racing-rw01n-8-p985975';
-    let pageUrl = 'https://www.cea.com.br/camisa-feminina-estampada-estrelas-com-bolso-manga-curta-verde-escuro-9942341-verde_escuro/p';
+    let pageUrl = 'https://www.dxracer.com.br/cadeira-dxracer-racing-rw01n-8-p985975';
+    //let pageUrl = 'https://example.com/';
+    //let pageUrl = 'https://www.lamudi.co.id/newdevelopments/';
+    //let pageUrl = 'https://www.cea.com.br/camisa-feminina-estampada-estrelas-com-bolso-manga-curta-verde-escuro-9942341-verde_escuro/p';
     
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(pageUrl, { waitUntil: 'networkidle2' });
 
-    const html = await page.content();
+    //const html = await page.content();
 
-    fs.writeFileSync("output.html", html, function(err) {
+    //Puppeteer nao suporta XPATH 2.0 (que da suporte a expressoes regulares)
+    const myXpath = "//*[matches(text(),'/(?:[1-9]\d{0,2}(?:\.\d{3})*),\d{2}/g')]";
+    // wait for element defined by XPath appear in page
+    await page.waitForXPath(myXpath);
+    // evaluate XPath expression of the target selector (it return array of ElementHandle)
+    let elHandle = await page.$x(myXpath);
+    // prepare to get the textContent of the selector above (use page.evaluate)
+    for (let i = 0; i<elHandle.length; i++){
+        console.log(await page.evaluate(el => el.textContent, elHandle[i]));
+    }
+
+    /*fs.writeFileSync("output.html", html, function(err) {
         if(err) {
             return console.log(err);
         }
@@ -37,7 +51,7 @@ const { measureMemory } = require('vm');
             });
         }
     });
-    
+
     //Clue 1
     let re = new RegExp(/R\$/g);
     matchList.forEach((object) => {
@@ -69,6 +83,6 @@ const { measureMemory } = require('vm');
 
     //console.log(matchList);
     console.log("Foram encontrados " + (matchList.length) +" valores em reais na página.");
-
+    */
     await browser.close();
 })();
