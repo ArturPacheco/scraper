e56f3969c58e4f76e6471151c23ce8413f8a4f2a
Artur Pacheco <arturpacheko@gmail.com>
2021-02-09 00:05:38 -0300

    Charts working with hourly results

--- prepareChart.js
+++ prepareChart.js
@@ -1,39 +1,54 @@
 const jsonData = require('./personal-products.json')
+const fs = require('fs');
 
 function generateChartData() {
 
+    var data = {}
     var chartData = []
 
+    //LIMPA OS TIMESTAMPS PARA DEIXAR APENAS OS DIAS E MONTAR AS LABELS DO CHART
+    var chartLabels = []
     jsonData.forEach(item => {
         if (item.runs) {
             item.runs.forEach(run => {
                 if (run !== null) {
                     let dayOnly = new Date(run.timeStamp)
-                    dayOnly = dayOnly.setHours(0, 0, 0, 0)
+                    dayOnly = dayOnly.setHours(dayOnly.getHours(), 0, 0, 0)
                     run.timeStamp = dayOnly
-                } else {
-                    chartData
+                    chartLabels.push(run.timeStamp)
                 }
-                
             })
         }
-        
     })
 
-    /*var daysBuffer = []
-    days.forEach(item => {
-        if (item !== null) {
-            let dayOnly = new Date(item)
-            dayOnly = dayOnly.setHours(0, 0, 0, 0)
-            daysBuffer.push(dayOnly)
-        } else {
-            daysBuffer.push(null)
+    chartLabels = new Set(chartLabels)
+    data.labels = Array.from(chartLabels)
+
+    //CRIA A ESTRUTURA DE OBJETOS PARA O GRAFICO
+    jsonData.forEach(item => {
+        var dataset = {}
+        if (item.runs) {
+            dataset.label = item.siteName
+            dataset.fill = false
+            var runData = []
+            var xY = {}
+            item.runs.forEach(run => {
+                xY = {}
+                if (run !== null) {
+                    xY.x = run.timeStamp
+                    xY.y = run.price[0]
+                    if ( chartLabels.has(run.timeStamp) ) {
+                        runData.push(xY)
+                    }
+                }
+            })
+            dataset.data = runData
+            chartData.push(dataset)
         }
-    })*/
+    })
+    data.datasets = chartData
 
-    //days = new Set(daysBuffer)
-
-    console.log(days)
+    fs.writeFileSync('chartData.json', JSON.stringify(data, null, 2)) //Escreve novos resultados no JSON
 }
 
 generateChartData()
